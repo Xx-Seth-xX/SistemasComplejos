b9fa21909dd34bc5c862434ba98a1e2358b397f8
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2023-03-16 17:30:36 +0100

    Changed to GLMakie.jl

--- scripts/vicsek_plots.jl
+++ scripts/vicsek_plots.jl
@@ -10,16 +10,21 @@ function execute_sim_dict(sim::SimulationParameters)
     return @strdict(time, va)
 end
 
-function plot_va_vs_time_at_various_η(N, L, r, celerity, η)
+function plot_va_vs_time_at_various_η(;N, L, r, celerity, η)
     duration = 1000
     Δt = 1
     folder = datadir("preliminary_analysis")
     configs = merge(@dict(L, r,  N, celerity, duration, Δt), @dict(η)) |> dict_list
     fig = Figure()
-    ax = fig(ax[1,1])
+    ax = Axis(fig[1,1])
+    ylims!(ax, 0,1)
     for config in configs
-        produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)
-        ax
+        data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
+        lines!(ax, data["time"], data["va"], label = "η = $(config[:η])")
     end
-
+    axislegend(ax)
+    safesave(plotsdir("preliminary_analysis", savename(@dict(L, r,  N, celerity, duration, Δt), "png")),
+               fig)
+    return fig
 end
+
