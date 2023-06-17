7fac8426083e1742229e890300615e01a3c81f03
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2023-06-17 02:24:01 +0200

    addes calculate_vs_angle

--- scripts/vicsek_plots.jl
+++ scripts/vicsek_plots.jl
@@ -7,6 +7,7 @@ using Statistics
 using StatsBase
 using CSV, CSVFiles
 using DataFrames
+using Colors
 
 update_theme!(fontsize = 30)
 
@@ -20,16 +21,16 @@ function execute_sim_keep_frames_dict(sim::SimulationParameters)
     return @strdict(time, flock)
 end
 
-function plot_va_vs_time_at_various_η(;N, L, η)
+function plot_va_vs_time(;N, L, η, ϕ)
     duration = 10_000
     folder = datadir("preliminary_analysis")
-    configs = merge(@dict(L, N, duration), @dict(η)) |> dict_list
+    configs = merge(@dict(L, N, duration), @dict(η), @dict(ϕ)) |> dict_list
     fig = Figure()
     ax = Axis(fig[1,1])
     ylims!(ax, 0,1)
     for config in configs
         data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
-        lines!(ax, data["time"], data["va"], label = "η = $(config[:η])")
+        lines!(ax, data["time"], data["va"], label = L"η = %$(config[:η]), ϕ = %$(config[:ϕ])")
     end
     axislegend(ax)
     safesave(plotsdir("preliminary_analysis", savename(@dict(L, N), "png")),
@@ -37,10 +38,10 @@ function plot_va_vs_time_at_various_η(;N, L, η)
     return fig
 end
 
-function animate_simulation(; N, L, η)
+function animate_simulation(; N, L, η, ϕ, duration = 20)
     framerate = 30
-    duration = 5000
-    config = @dict N L duration η
+    duration = framerate * duration
+    config = @dict N L duration η ϕ
     fig = Figure()
     ax = Axis(fig[1,1])
     xlims!(ax, 0, L)
@@ -57,8 +58,24 @@ function animate_simulation(; N, L, η)
     ys = @lift(getindex.($positions,2))
     us = @lift(getindex.($directions,1))
     vs = @lift(getindex.($directions,2))
+    function get_n_before_i(v::Vector, n, i)  
+        if n > i
+            n = i
+        end
+        vout = reduce(hcat, v[(i - n + 1):i])
+        return vout
+    end
 
+    #rows are the particles and the columns are the times
+    tails = @lift(get_n_before_i(data["flock"], 20, $counter) .|> Vicsek.position)
+    
     arrows!(ax, xs, ys, us, vs, color = :black, size = L/2)
+
+    for i in 1:size(tails[])[1]
+        tail = @lift(($tails)[i,:])
+        @show tail
+        scatter!(ax, tail, markersize = 6, markerspace = :pixel, color = RGBA(0,1,0,0.3))
+    end
 
     p = Progress(duration + 1, desc = "Rendering simulation...", dt=1.0, showspeed = true)
 
@@ -75,6 +92,28 @@ function calculate_correlation_time(; N, L)
     folder = datadir("preliminary_analysis", "correlation_time")
     config = @dict(N, L, duration, Δt, η)
     println("Calculating correlation time for N = $(config[:N]), L = $(config[:L]), η = $(config[:η])")
+    data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
+
+    #We get va from data
+    va = data["va"]
+
+    #We ignore the first 10_000 va points so we can get rid of the transient behaviour
+    va = va[300:end]
+
+    #We calculate the autocorrelation function
+    acf = autocor(va, 0:500)
+    #We calculate the correlation time as the first data point that goes below 1/e
+    τ = findfirst(x -> x < exp(-1), acf)
+    println("Correlation time for given config is: $(τ)")
+    return τ
+end
+function calculate_correlation_time_angle(; N, L, ϕ = π, η = 2)
+    η = 2
+    duration = 1_000_000
+    Δt = 1 #We need to keep every frame so we can calculate correlation time
+    folder = datadir("preliminary_analysis", "correlation_time")
+    config = @dict(N, L, duration, Δt, η, ϕ)
+    println("Calculating correlation time for N = $(config[:N]), L = $(config[:L]), η = $(config[:η]), ϕ = $(config[:ϕ])")
     data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
 
     #We get va from data
@@ -139,6 +178,39 @@ function calculate_va_vs_noise(;L, N)
 
             #The duration will be the number of data points times the time step plus 300 to eliminate the transient behaviour
             duration = number_of_data_points * Δt + 300
+            config = @dict(L, N, duration, Δt, η)
+            data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
+            va = data["va"]
+            @show mean(va[end-number_of_data_points:end])
+            @show std(va[end-number_of_data_points:end])*2
+            #We calculate the mean of the va for each noise value
+            va = va[end-number_of_data_points:end]
+            @show mean(va)
+            @show std(va)*2 / (sqrt(length(va) - 1))
+            @show std(va)*2 
+            push!(va_mean, mean(va))
+            push!(va_var, std(va, corrected = false))
+            push!(va_error, std(va)*2 / sqrt(length(va) - 1)) 
+        end
+        #we save the data to csv
+        safesave(datadir("va_vs_noise", savename(@dict(L, N), "csv")), DataFrame(@strdict(η, va_mean, va_error, va_var)))
+    end
+end
+function calculate_va_vs_angle(;L, N, η)
+    #We parametrise the noise from 0 to 5 at 0.1 intervals
+    L = float(L)
+    ϕ = range(0, π, length = 15)
+    number_of_data_points = 3000
+    for (L, N) in zip(L, N)
+        va_mean = Float64[]
+        va_error = Float64[]
+        va_var = Float64[]
+        folder = datadir("raw_data")
+        #Adaptative time step so we always use 2 times the correlation time
+        Δt = 2*calculate_correlation_time_angle(;N = N, L = L, ϕ = π/2, η = 2)
+        for ϕ in ϕ
+            #The duration will be the number of data points times the time step plus 300 to eliminate the transient behaviour
+            duration = number_of_data_points * Δt + 500
             config = @dict(L, N, duration, Δt, η)
             data = produce_or_load(execute_sim_dict, SimulationParameters(;config...), folder)[1]
             va = data["va"]
